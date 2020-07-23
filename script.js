802d8eaf6d67b8407125206a9f37861e6f3e2424
dogsincare520 <68648620+dogsincare520@users.noreply.github.com>
2020-07-23 20:29:26 +0800

    0723-10

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
+$('body').scrollspy({ target: '#myNavbar'})
+$("#gotop").click(function(){
+    jQuery("html,body").animate({
+        scrollTop:0
+    },1000);
+});
+$(window).scroll(function() {
+    if ( $(this).scrollTop() > 300){
+        $('#gotop').fadeIn("fast");
+    } else {
+        $('#gotop').stop().fadeOut("fast");
+    }
+});
+
 $('.navbar-nav li a').on('click', function(){
     if(!$( this ).hasClass('dropdown-toggle')){
         $('.navbar-collapse').collapse('hide');
     }
 });
-
-/*圖片輪播*/
-
-$("#myCarousel").carousel({
-			interval :3000,
-		});
-/*
-$('.navbar a').on('click', function()
-{
-//點擊時，如果已經沒有下一層選單的話，則再關閉navbar
-if($(this).attr("data-toggle") == undefined)
-{
-$('.navbar-toggle').click();
-}
-});    */
