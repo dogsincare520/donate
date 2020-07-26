42b4859c2dcd36fd934f516623a93d0cc1d12697
dogsincare520 <68648620+dogsincare520@users.noreply.github.com>
2020-07-26 16:14:09 +0800

    0726

--- script.js
+++ script.js
@@ -21,3 +21,15 @@ $('.navbar-nav li a').on('click', function(){
 $("#myCarousel").carousel({
 			interval :3000,
 		});
+
+$(function(){
+  var hash = window.location.hash;
+  hash && $('ul.nav a[href="' + hash + '"]').tab('show');
+
+  $('.nav-tabs a').click(function (e) {
+    $(this).tab('show');
+    var scrollmem = $('body').scrollTop() || $('html').scrollTop();
+    window.location.hash = this.hash;
+    $('html,body').scrollTop(scrollmem);
+  });
+});
